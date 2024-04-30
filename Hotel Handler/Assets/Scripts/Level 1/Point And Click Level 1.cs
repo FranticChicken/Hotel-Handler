008e93797e0a1c8dbfb6ca31d5a64a0c5f318327
FranticChicken <emilyperron2014@gmail.com>
2024-04-30 03:22:36 -0400

    AI movement set up but not finished

--- Hotel Handler/Assets/Scripts/Level 1/Point And Click Level 1.cs
+++ Hotel Handler/Assets/Scripts/Level 1/Point And Click Level 1.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class PointAndClickLevel1 : MonoBehaviour
 {
+    public AIDestinationSetter aiDestSet;
+    private Vector3 ourTarget;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,15 @@ public class PointAndClickLevel1 : MonoBehaviour
     void Update()
     {
         
+            //use 0 for left mouse button and 1 for right mouse button
+            if (Input.GetMouseButtonDown(1))
+        {
+            ourTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ourTarget.z = transform.position.z;
+
+            aiDestSet.target = ourTarget;
+        }
+
+           
     }
 }
