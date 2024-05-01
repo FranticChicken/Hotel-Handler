e6e8efe81a3290195c912cf7d1a8038e289dca03
Emily <emilyperron2014@gmail.com>
2024-05-01 01:33:45 -0400

    Fixed pathfinding + added mov locations

--- Hotel Handler/Assets/Scripts/Level 1/Point And Click Level 1.cs
+++ Hotel Handler/Assets/Scripts/Level 1/Point And Click Level 1.cs
@@ -7,6 +7,13 @@ public class PointAndClickLevel1 : MonoBehaviour
 {
     public AIDestinationSetter aiDestSet;
     private Vector3 ourTarget;
+    public Transform room1Detect;
+    public Transform room2Detect;
+    public Transform frontDeskDetect;
+    public Transform room1StandPos;
+    public Transform room2StandPos;
+    public Transform frontDeskStandPos; 
+
    
     // Start is called before the first frame update
     void Start()
@@ -17,14 +24,34 @@ public class PointAndClickLevel1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
             //use 0 for left mouse button and 1 for right mouse button
-            if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1))
         {
-            ourTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            ourTarget.z = transform.position.z;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
-            aiDestSet.target = ourTarget;
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.transform == room1Detect)
+                {
+                    aiDestSet.target = room1StandPos;
+                }
+                else if (hit.collider.gameObject.transform == room2Detect)
+                {
+                    aiDestSet.target = room2StandPos;
+                }
+                else if (hit.collider.gameObject.transform == frontDeskDetect)
+                {
+                    aiDestSet.target = frontDeskStandPos;
+                }
+            }
+            else
+            {
+                Debug.Log("no collider");
+            }
+
+            
         }
 
            
